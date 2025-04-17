b19f99d5064bb33277a5d7d1dcc774f5c31b8f96
Whiston Kendrick Borja Reyna <kenwhistonuns2005@gmail.com>
2025-04-17 13:35:03 -0500

    [FEATURE] Added components expo-maps-native and render map en search task

--- app/(tabs)/buscarTareas/index.tsx
+++ app/(tabs)/buscarTareas/index.tsx
@@ -1,9 +1,38 @@
 import { StyleSheet, Image, Platform, View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function BuscarTareasScreen() {
   return (
-    <View className='flex-1 justify-center items-center' >
-      <Text className='text-6xl text-green-800' >Explore Screen</Text>
-    </View >
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          }}
+          title="My Location"
+          description="This is a marker in San Francisco"
+        />
+      </MapView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
